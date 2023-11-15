359e2577912a4f15a0eb188d5390536fc8810efd
Administrador <Administrador@2DAM01>
2023-11-15 19:07:10 +0100

    Cuarto cambio

--- src/cosa/Cosa.java
+++ src/cosa/Cosa.java
@@ -6,5 +6,6 @@ public static void main (String[]args) {
 	System.out.println("Primer Cambio");
 	System.out.println("Segundo Cambio");
 	System.out.println("Tercer cambio");
+	System.out.println("Cuarto cambio");
 }
 }
